2f59f53765765e05524ebc34d865f3fcda438299
aikezhanakhmatova <alinaakhmatova77@gmail.com>
2023-07-06 16:17:05 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,8 @@ public class Main {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("Aikejan");
+        System.out.println("aika eje");
+
     }
 }
